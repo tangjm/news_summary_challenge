3d270bd48c349b7cc67a554c982f9d76e40f7c8a
tangjm <jaredtang@hotmail.co.uk>
2021-11-26 17:59:40 +0000

    Added psasing test for SummaryPage

--- src/Tests/SummaryPage.test.js
+++ src/Tests/SummaryPage.test.js
@@ -1,16 +1,17 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import SummaryPage from '../Components/SummaryPage';
 import sampleArticles from '../utils/sampleArticles.json';
 
-xtest(`Article Block matches snapshot`, () => {
-	const summaryPage = render(<SummaryPage />);
-	expect(summaryPage).toMatchSnapshot();
-})
-
 jest.mock(`../Components/ArticleBlock`, () => {
 	return function MockArticleBlock() {
 		return <div>Mock ArticleBlock Component</div>;
+	};
+});
+
+jest.mock(`../Components/ArticleText`, () => {
+	return function MockArticleText() {
+		return <div>Mock ArticleText Component</div>;
 	};
 });
 
@@ -29,8 +30,21 @@ describe(`SummaryPage test suite`, () => {
 		const testArticleBlock = screen.getByText(/mock articleblock component/i);
 
 		expect(testArticleBlock).toBeInTheDocument();
-	})
+	});
 
-	xtest(`it should render ArticleText`, () => {
+	test(`it should render ArticleText`, () => {
+		const testArticleId = "testId";
+		const articles = sampleArticles.data.response.results;
+		articles[0].id = testArticleId;
+
+		render(
+			<BrowserRouter>
+				<SummaryPage articleArr={articles} />
+			</BrowserRouter>
+		);
+
+		const testArticleText = screen.getByText(/mock articletext component/i);
+
+		expect(testArticleText).toBeInTheDocument();
 	})
 })

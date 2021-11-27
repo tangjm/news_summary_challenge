7744d662a299f60a5c89be2525739d6646dfceb8
tangjm <jaredtang@hotmail.co.uk>
2021-11-27 16:44:05 +0000

    Removed ArticleTitle component; incorporated it's functionality into the ArticleHeadline

--- src/Components/ArticleHeadline.jsx
+++ src/Components/ArticleHeadline.jsx
@@ -1,10 +1,19 @@
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 
-const ArticleHeadline = ({ headline, id }) => {
+const ArticleHeadline = ({ headline, id, url, displaySummary }) => {
 
 	const articleTitle = headline;
 	const articleId = id;
+	const articleUrl = url;
+
+	if (displaySummary) {
+		return (
+			<a href={articleUrl} target="_blank" rel="noreferrer">
+				<h1>{articleTitle}</h1>
+			</a>
+		)
+	}
 
 	return (
 		<NavLink to={{
@@ -19,6 +28,8 @@ const ArticleHeadline = ({ headline, id }) => {
 ArticleHeadline.propTypes = {
 	headline: PropTypes.string,
 	id: PropTypes.string,
+	url: PropTypes.string,
+	displaySummary: PropTypes.bool
 }
 
 export default ArticleHeadline;

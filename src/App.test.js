2ca28c980303aa5ed804f9c83a81143822a41ca1
tangjm <jaredtang@hotmail.co.uk>
2021-11-27 10:55:17 +0000

    Added failing test for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 import App from './App';
+// import { sampleArticles as mockArticles } from './utils/sampleArticles.json';
+const mockArticles = require(`./utils/sampleArticles.json`);
 
-xtest('renders DFA Github link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Digital Futures Academy/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+// jest.mock("axios", () => {
+//   return {
+//     __esModule: true,
+//     default: jest.fn().mockResolvedvalue(mockArticles)
+//   }
+// })
+jest.mock('axios', () => jest.fn(() => Promise.resolve(mockArticles)));
+
+
+describe(`App.js test suite`, () => {
+  beforeEach(() => {
+
+  })
+
+  test('renders HeadlinesPage', async () => {
+    act(() => {
+      render(<App />);
+    })
+
+    const testApp = screen.getByText(/B\.1\.1\.529 Covid variant ‘most worrying we’ve seen’, says top UK medical adviser/i);
+
+
+    await waitFor(() => {
+      expect(testApp).toBeInTheDocument();
+      expect(appComponent).toContainElement("p");
+
+    })
+  });
+})
+
+

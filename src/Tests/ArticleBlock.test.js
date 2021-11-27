5e09f9b20072a3058c9457ed6bfe720d60832345
tangjm <jaredtang@hotmail.co.uk>
2021-11-27 16:54:22 +0000

    Simplified ArticleBlock test as we will are no longer conditionally rendering components

--- src/Tests/ArticleBlock.test.js
+++ src/Tests/ArticleBlock.test.js
@@ -2,12 +2,6 @@ import { render, screen } from '@testing-library/react';
 import ArticleBlock from '../Components/ArticleBlock';
 import ArticleModel from '../utils/ArticleModel';
 
-jest.mock(`../Components/ArticleTitle`, () => {
-	return function MockArticleTitle() {
-		return <h1>Mock ArticleTitle Component</h1>
-	}
-})
-
 jest.mock(`../Components/ArticleHeadline`, () => {
 	return function MockArticleHeadline() {
 		return <h1>Mock ArticleHeadline Component</h1>
@@ -15,7 +9,7 @@ jest.mock(`../Components/ArticleHeadline`, () => {
 })
 
 describe(`ArticleBlock test suite`, () => {
-	describe(`Tests for when there is no displaySummary`, () => {
+	describe(`Render tests`, () => {
 		test(`it should render ArticleHeadline`, () => {
 			const testArticle = new ArticleModel("Mock id", "Mock title", "Mock url", "Mock img", "Mock text");
 
@@ -47,49 +41,14 @@ describe(`ArticleBlock test suite`, () => {
 		})
 	})
 
-	describe(`Tests for when displaySummary is true`, () => {
-		test(`it should render ArticleTitle`, () => {
-			const testArticle = new ArticleModel("Mock id", "Mock title", "Mock url", "Mock img", "Mock text");
-			const displaySummary = true;
+	test(`it should render an error message if an error object is received`, () => {
+		const testArticle = { error: "error message" };
+		const displaySummary = true;
 
-			render(<ArticleBlock article={testArticle} displaySummary={displaySummary} />);
+		render(<ArticleBlock article={testArticle} displaySummary={displaySummary} />);
 
-			const testArticleTitle = screen.getByText("Mock ArticleTitle Component");
+		const testErrorObj = screen.getByText(/error message/i);
 
-			expect(testArticleTitle).toBeInTheDocument();
-		});
-
-		test(`it should render a thumbnail image`, () => {
-			const testArticle = new ArticleModel("Mock id", "Mock title", "Mock url", "Mock img", "Mock text");
-			const displaySummary = true;
-
-			render(<ArticleBlock article={testArticle} displaySummary={displaySummary} />);
-
-			const testThumbnail = screen.getByRole("img");
-
-			expect(testThumbnail).toBeInTheDocument();
-		})
-
-		test(`it should render a thumbnail image with the correct src`, () => {
-			const testArticle = new ArticleModel("Mock id", "Mock title", "Mock url", "Mock img", "Mock text");
-			const displaySummary = true;
-
-			render(<ArticleBlock article={testArticle} displaySummary={displaySummary} />);
-
-			const testThumbnail = screen.getByRole("img");
-
-			expect(testThumbnail).toHaveAttribute("src", testArticle.thumbnail)
-		})
-
-		test(`it should render an error message if an error object is received`, () => {
-			const testArticle = { error: "error message" };
-			const displaySummary = true;
-
-			render(<ArticleBlock article={testArticle} displaySummary={displaySummary} />);
-
-			const testErrorObj = screen.getByText(/error message/i);
-
-			expect(testErrorObj).toBeInTheDocument();
-		});
-	})
+		expect(testErrorObj).toBeInTheDocument();
+	});
 })
